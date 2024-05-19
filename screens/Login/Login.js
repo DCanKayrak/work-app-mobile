ccd967008093a2031601717e49a4fe6570d39da1
DANYAL CAN KAYRAK <dancankan@gmail.com>
2024-05-19 23:57:18 +0300

    chore: login & register fetch operations

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -4,6 +4,47 @@ import { TextInput } from 'react-native-gesture-handler'
 import CustomTextInput from '../../components/CustomTextInput/CustomTextInput'
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const navigate = useNavigate();
+
+    const sendRequest = () => {
+        PostWithoutAuth(("https://localhost:7048/api/Auth/Login"),{
+            email: email,
+            password:password
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                if(result.success){
+                    setError(null)
+                    localStorage.setItem("tokenKey", result.data.token);
+                    setSuccess(result.message)
+                    navigate("/")
+                }
+                else {
+                    setSuccess(null)
+                    console.log(result.message)
+                    setError(result.message)
+                }
+                
+            }).catch((err) => console.log(err.message))
+    }
+
+
+    const handleLogin = () => {
+        sendRequest()
+        setEmail("")
+        setPassword("")
+    }
+
+    useEffect(() => {
+    },[email,password,error])
+
+
     return (
         <View>
             <Text>Login</Text>
--- screens/Register/Register.js
+++ screens/Register/Register.js
@@ -2,6 +2,59 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Register = () => {
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
+    const [repassword, setRepassword] = useState("");
+
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const navigate = useNavigate();
+
+    const sendRequest = () => {
+        PostWithoutAuth(("https://localhost:7048/api/Auth/Register"),{
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            password:password
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                if(result.success){
+                    setError(null)
+                    setSuccess(result.message)
+                    navigate("/")
+                }
+                else {
+                    setSuccess(null)
+                    console.log(result.message)
+                    setError(result.message)
+                }
+                
+            }).catch((err) => console.log(err.message))
+    }
+
+
+    const handleRegister = () => {
+        if (password === repassword) {
+            sendRequest()
+        }
+        else {
+            console.log("Şifreler aynı değil.")
+        }
+        
+        setEmail("")
+        setFirstName("")
+        setLastName("")
+        setPassword("")
+    }
+
+    useEffect(() => {
+    },[email,password,error])
+
+
     return (
         <View>
             <Text>Register</Text>
